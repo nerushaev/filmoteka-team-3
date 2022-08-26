6036515559dd0d182d3246e4358b16f6c2802856
Oleksandr Kucheryavenko <kucheryavenko.oleksandr@gmail.com>
2022-08-26 23:14:53 +0300

    feature/openCloseModalInfo
    Добавил ф-цию открытия и закрытия модалки инфо, временно открывается через кнопку. После готовых карточек добавлю открытие на карточку.

--- src/js/openCloseModalInfo.js
+++ src/js/openCloseModalInfo.js
@@ -1,34 +1,32 @@
-// Refs
-const backdropEl = document.querySelector('.backdrop');
-const btnCloseModal = document.querySelector('.js-btn-close');
-const cardEl = document.querySelector('.js-card')
-// Cl El
-console.log(backdropEl);
-console.log(btnCloseModal);
-console.log(cardEl);
-//Evt
-cardEl.addEventListener('click', onOpenModal);
-btnCloseModal.addEventListener('click', onCloseModal);
-backdropEl.addEventListener('click', onBackdropClick)
+import { refs } from './refs';
+
+console.log(refs.backdropEl);
+console.log(refs.modalInfoEL);
+console.log(refs.closeBtnModalInfo);
+console.log(refs.watchedBtnModalInfo);
+console.log(refs.queueBtnModalInfo);
+console.log(refs.imitationCard);
+
+// Слушатели
+refs.imitationCard.addEventListener('click', onOpenModalInfo);
+refs.closeBtnModalInfo.addEventListener('click', onCloseModalInfo);
+refs.backdropEl.addEventListener('click', onBackdropClick)
 
 
 // Ф-ция открывает модальное окно с инфармацией о выбраном фильме, при нажатии на карточу с фильмом 
-function onOpenModal() {
-    // console.log("Modal is open!");
+function onOpenModalInfo() {
     window.addEventListener('keydown', onEscKeyPress);
-    backdropEl.classList.remove('backdrop__is-hidden');
+    refs.backdropEl.classList.remove('backdrop__is-hidden');
 }
 // Ф-ция закрывает модальное окно с инфармацией о фильме, при нажатии на кнопку закрытия
-function onCloseModal() {
-    // console.log("Modal is close!");
+function onCloseModalInfo() {
     window.removeEventListener('keydown', onEscKeyPress);
-    backdropEl.classList.add('backdrop__is-hidden');
+    refs.backdropEl.classList.add('backdrop__is-hidden');
 }
 // Ф-ция закрывает модальное окно с инфармацией о фильме, при нажатии вне модалки (на бэкдроп)
 function onBackdropClick(evt) {
     if (evt.currentTarget === evt.target) {
-        // console.log("This function onBackdropClick");
-        onCloseModal();
+        onCloseModalInfo();
     }
 }
 // Ф-ция закрывает модальное окно с инфармацией о фильме, при нажатии на Esc
@@ -36,6 +34,6 @@ function onEscKeyPress(evt) {
     const ESC_KEY_CODE = 'Escape';
 
     if (evt.code === ESC_KEY_CODE) {
-        onCloseModal();
+        onCloseModalInfo();
     }
 }
--- src/js/refs.js
+++ src/js/refs.js
@@ -2,7 +2,13 @@ const refs = {
   API_KEY: 'ade224e438a5cac910232dacab4d697a',
   POPULAR_URL: 'https://api.themoviedb.org/3/trending/all/week',
   GENRES_URL: 'https://api.themoviedb.org/3/genre/movie/list',
-  SEARCH_URL: 'https://api.themoviedb.org/3/search/movie'
+  SEARCH_URL: 'https://api.themoviedb.org/3/search/movie',
+  backdropEl: document.querySelector('.js-backdrop'),
+  modalInfoEL: document.querySelector('.js-modal__info'),
+  closeBtnModalInfo: document.querySelector('.js-btn__info--close'),
+  watchedBtnModalInfo: document.querySelector('.js-btn__info--watched'),
+  queueBtnModalInfo: document.querySelector('.js-btn__info--queue'),
+  imitationCard: document.querySelector('.js-card'),
 };
 
 
@@ -11,7 +17,5 @@ const userSearch = {
   userSearchInputRef: document.querySelector('.header__input'),
   searchButton: document.querySelector('.form-icon')
 };
-
-
 
 export { refs, userSearch };
