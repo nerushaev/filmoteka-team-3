b9a80b5f86a05b2d24a551f4392ab93f96b35d42
Oleksandr Kucheryavenko <kucheryavenko.oleksandr@gmail.com>
2022-08-27 21:46:58 +0300

    task/openCloseModal

--- src/js/openCloseModalInfo.js
+++ src/js/openCloseModalInfo.js
@@ -1,20 +1,21 @@
 import { refs } from './refs';
 import { generateMarkupModalInfo } from './infoForModalMarkup';
 
-// Слушатели
-const testClick = document.querySelector('.js-gallery__set');
-console.log(testClick);
+const DELAY = 250;
 
-testClick.addEventListener('click', onOpenModalInfo);
+// Слушатели
+refs.gallerySetEL.addEventListener('click', onOpenModalInfo);
 refs.closeBtnModalInfo.addEventListener('click', onCloseModalInfo);
 refs.backdropEl.addEventListener('click', onBackdropClick);
 
 // Ф-ция открывает модальное окно с инфармацией о выбраном фильме, при нажатии на карточу с фильмом 
 function onOpenModalInfo(evt) {
-
+    if (evt.target.nodeName === 'UL') {
+        return;
+    }
     window.addEventListener('keydown', onEscKeyPress);
     refs.backdropEl.classList.remove('backdrop__is-hidden');
-    appendInfoForModalMurkup();
+    appendInfoForModalMarkup();
 }
 // Ф-ция закрывает модальное окно с инфармацией о фильме, при нажатии на кнопку закрытия
 function onCloseModalInfo() {
@@ -36,11 +37,13 @@ function onEscKeyPress(evt) {
         onCloseModalInfo();
     }
 }
-
-function appendInfoForModalMurkup() {
+// Ф-ция добавляет разметку с информацией в модальное окно
+function appendInfoForModalMarkup() {
     refs.containerModalInfo.insertAdjacentHTML('beforeend', generateMarkupModalInfo());
 }
-
+// Ф-ция очищает разметку с информацией в модальном окне (DELAY = $Duration)
 function clearInfoForModalMarkup() {
-    refs.containerModalInfo.innerHTML = '';
+    setTimeout(() => {
+        refs.containerModalInfo.innerHTML = '';
+    }, DELAY);
 }
