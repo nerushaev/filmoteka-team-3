185197013b8e5de244a0d5cc0f5e5f5c81aa080d
Sova-Anton <anton.sova.goit@gmail.com>
2022-08-27 22:20:38 +0300

    fix Poster

--- src/js/getPopularFilms.js
+++ src/js/getPopularFilms.js
@@ -36,14 +36,14 @@ const homePageGallery = document.querySelector(".gallery__set");
 function renderMarkupPopularFilms(data) {
   const markup = data.map(
       ({
-        backdrop_path,
+        poster_path,
         genre_ids,
         release_date,
         title,
       }) => {
       return `<li class="gallery__item">
                 <a  class="gallery__item__link" target="_blank" rel="noopener noreferrer">
-                    <img src="${refs.IMG_URL}${backdrop_path}" alt="${title}" class="gallery__item__img">
+                    <img src="${refs.IMG_URL}${poster_path}" alt="${title}" class="gallery__item__img">
                     <div class="gallery__item__text">
                         <h3 class="gallery__item__title">${title}</h3>
                         <div class="gallery__item__descr">
