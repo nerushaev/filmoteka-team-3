a8b21060d3afeeec40f144f08efa9c885d233909
analepekha <maiboroda0103@gmail.com>
2022-08-28 23:10:26 +0300

    update

--- src/js/parseGenres.js
+++ src/js/parseGenres.js
@@ -3,14 +3,14 @@ import { refs } from './refs';
 export function renderMarkupGenres(idFilmGenre) {
   const genresList = localStorage.getItem(refs.STORAGE_KEY);
   const genresListFind = JSON.parse(genresList).genresData;
-console.log("genresListFind",genresListFind);
-  console.log(idFilmGenre);
+// console.log("genresListFind",genresListFind);
+  // console.log(idFilmGenre);
 
   const numberGenres = idFilmGenre.length;
   
   const ganres = idFilmGenre.map((element) =>
     (genresListFind.find(genre => genre.id == element)).name).join(', ')
-  console.log(ganres);
+  // console.log(ganres);
 
   return (ganres);  
   }
