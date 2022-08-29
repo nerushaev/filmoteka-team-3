6c5f0102eca9b19dd9c1745961bafb8228c264a5
Sova-Anton <anton.sova.goit@gmail.com>
2022-08-29 16:39:14 +0300

    +

--- src/js/modalInfo/appModalInfo.js
+++ src/js/modalInfo/appModalInfo.js
@@ -1,7 +1,7 @@
 import { refs } from '../refs';
 import storage from '../storage';
 import { generateMarkupModalInfo } from './markupModalInfo';
- 
+
 const DELAY = 250;
 
 // Слушатели
@@ -12,47 +12,52 @@ refs.backdropEl.addEventListener('click', onBackdropClick);
 // Ф-ция открывает модальное окно с инфармацией о выбраном фильме, при нажатии на иконку с фильмом
 // (нужно на карточу с фильмом - ! не выходит поймать evt на LI, что бы взять id с LI, а не IMG!)
 async function onOpenModalInfo(evt) {
-    if (evt.target.nodeName !== 'IMG') {
-        return;
-    }
-    const loadPopMovies = await storage.load(refs.LS_KEY_POPULAR_MOVIE);
-    let idMovie = Number(evt.target.dataset.id);
-    const selectedMovie = loadPopMovies.find(loadMovie => loadMovie.id === idMovie);
-    appendInfoForModalMarkup(selectedMovie);
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  const loadPopMovies = await storage.load(refs.LS_KEY_POPULAR_MOVIE);
+  let idMovie = Number(evt.target.dataset.id);
+  const selectedMovie = loadPopMovies.find(
+    loadMovie => loadMovie.id === idMovie
+  );
+  appendInfoForModalMarkup(selectedMovie);
 
-    window.addEventListener('keydown', onEscKeyPress);
-    refs.backdropEl.classList.remove('backdrop__is-hidden');
+  window.addEventListener('keydown', onEscKeyPress);
+  refs.backdropEl.classList.remove('backdrop__is-hidden');
 }
 
 // Ф-ция закрывает модальное окно с инфармацией о фильме, при нажатии на кнопку закрытия
 function onCloseModalInfo() {
-    window.removeEventListener('keydown', onEscKeyPress);
-    refs.backdropEl.classList.add('backdrop__is-hidden');
-    clearInfoForModalMarkup();
+  window.removeEventListener('keydown', onEscKeyPress);
+  refs.backdropEl.classList.add('backdrop__is-hidden');
+  clearInfoForModalMarkup();
 }
 // Ф-ция закрывает модальное окно с инфармацией о фильме, при нажатии вне модалки (на бэкдроп)
 function onBackdropClick(evt) {
-    if (evt.currentTarget === evt.target) {
-        onCloseModalInfo();
-    }
+  if (evt.currentTarget === evt.target) {
+    onCloseModalInfo();
+  }
 }
 // Ф-ция закрывает модальное окно с инфармацией о фильме, при нажатии на Esc
 function onEscKeyPress(evt) {
-    const ESC_KEY_CODE = 'Escape';
+  const ESC_KEY_CODE = 'Escape';
 
-    if (evt.code === ESC_KEY_CODE) {
-        onCloseModalInfo();
-    }
+  if (evt.code === ESC_KEY_CODE) {
+    onCloseModalInfo();
+  }
 }
 
 // Ф-ция добавляет разметку с информацией в модальное окно
 function appendInfoForModalMarkup(selectedMovie) {
-    refs.containerModalInfo.insertAdjacentHTML('beforeend', generateMarkupModalInfo(selectedMovie));
+  refs.containerModalInfo.insertAdjacentHTML(
+    'beforeend',
+    generateMarkupModalInfo(selectedMovie)
+  );
 }
 
 // Ф-ция очищает разметку с информацией в модальном окне (DELAY = $Duration)
 function clearInfoForModalMarkup() {
-    setTimeout(() => {
-        refs.containerModalInfo.innerHTML = '';
-    }, DELAY);
+  setTimeout(() => {
+    refs.containerModalInfo.innerHTML = '';
+  }, DELAY);
 }
