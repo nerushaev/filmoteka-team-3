33fbeb010dc2c4404330fcd5b21f103b61b22d5f
Sova-Anton <anton.sova.goit@gmail.com>
2022-08-30 20:05:33 +0300

    thema

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import './js/getPopularFilms.js';
 import './js/loadHomePage';
 import './js/loader.js';
 
-
 import './js/getGenresList.js';
 import './js/getMovie.js';
 
 import './js/getGenresList.js';
 import './js/pagination.js';
 
-import './js/modalInfo/appModalInfo'; //  JS for open or close modal for information about the movie 
-
+import './js/modalInfo/appModalInfo'; //  JS for open or close modal for information about the movie
+import './js/thema';
 import './js/modal';
 // import './js/modalInfo/btnModalLibrary';
 import './js/loadQueryFilms.js';
