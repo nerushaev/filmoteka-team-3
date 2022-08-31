7d2975139ba270245ee8e3367b186bb146689af9
EkaterinaZi <kat.zinovik@gmail.com>
2022-08-31 18:38:12 +0100

    modal fix

--- src/js/modalInfo/btnModalLibrary.js
+++ src/js/modalInfo/btnModalLibrary.js
@@ -1,31 +1,35 @@
 import { refs } from "../refs";
-import { renderMarkupGenres } from '../parseGenres';
 
 const btnQueueLibrary = document.querySelector('#btn__info--queue');
 const btnWatchedLibrary = document.querySelector('#btn__info--watched');
 btnQueueLibrary.addEventListener('click', addToQueueList)
 btnWatchedLibrary.addEventListener('click', addToWatchedList)
-console.log(btnQueueLibrary)
-const arrIdQueue = []
-const arrIdWatched = []
 
 function addToQueueList(){
+const arrIdQueue = JSON.parse(localStorage.getItem(refs.LS_KEY_QUERY_MOVIE) || `[]`)
 const filmIdQueue = Number(document.querySelector('.film-screen').id)
-console.log(filmIdQueue)
-
+const sss = arrIdQueue.find(obj => obj.id === filmIdQueue)
+if(sss){
+console.log('fffffffff')
+return
+}
 const arrFilms = JSON.parse(localStorage.getItem(refs.LS_KEY_POPULAR_MOVIE));
 let arrayId = arrFilms.find(obj => obj.id === filmIdQueue)
-let a = arrIdQueue.push(arrayId)
+arrIdQueue.push(arrayId)
 localStorage.setItem(refs.LS_KEY_QUERY_MOVIE, JSON.stringify(arrIdQueue))
 }
 
-function addToWatchedList(){
-    const filmIdWatched = Number(document.querySelector('.film-screen').id)
-    console.log(filmIdWatched)
-    
-    const arrFilms = JSON.parse(localStorage.getItem(refs.LS_KEY_POPULAR_MOVIE));
-    let arrayId = arrFilms.find(obj => obj.id === filmIdWatched)
-    let a = arrIdWatched.push(arrayId)
-    localStorage.setItem(refs.LS_KEY_WATCH_MOVIE, JSON.stringify(arrIdWatched))
-
+function addToWatchedList(){ 
+const arrIdWatched = JSON.parse(localStorage.getItem(refs.LS_KEY_WATCH_MOVIE) || `[]`)
+const filmIdWatched = Number(document.querySelector('.film-screen').id)
+const sss = arrIdWatched.find(obj => obj.id === filmIdWatched)
+if(sss){
+console.log('fffffffff')
+return
 }
+    const arrFilms = JSON.parse(localStorage.getItem(refs.LS_KEY_POPULAR_MOVIE))
+    let arrayId = arrFilms.find(obj => obj.id === filmIdWatched)
+    arrIdWatched.push(arrayId) 
+    localStorage.setItem(refs.LS_KEY_WATCH_MOVIE, JSON.stringify(arrIdWatched))
+}
+  
